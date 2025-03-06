33b75104697e4255796e9ca553d1279b81caca7b
arhan <anup.arhan.das@gmail.com>
2025-03-06 19:03:34 +0530

    sound effects optimized

--- public/script.js
+++ public/script.js
@@ -71,6 +71,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     try {
       const audio = new Audio("/sounds/message-sent.mp3");
       audio.volume = 0.5;
+      audio.currentTime = 0.03; // Start playing from 0.03 seconds
       audio.play().catch((error) => {
         console.log("Error playing sent sound:", error);
       });
@@ -83,6 +84,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     try {
       const audio = new Audio("/sounds/message-recieved.mp3");
       audio.volume = 1;
+      audio.currentTime = 0.03; // Start playing from 0.03 seconds
       audio.play().catch((error) => {
         console.log("Error playing received sound:", error);
       });
@@ -96,6 +98,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     try {
       const audio = new Audio("/sounds/moan.mp3");
       audio.volume = 0.5;
+      audio.currentTime = 0.13; // Start playing from 0.13 seconds
       audio.play().catch((error) => {
         console.log("Error playing counter update sound:", error);
       });
